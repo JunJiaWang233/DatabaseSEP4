1b4847844591ba64cb2df5f3b0a97c248f8017ed
Rafal Pierscieniak <279471@via.dk>
2020-11-01 16:03:21 +0100

    spring boot web project inicialization

---
+++ src/main/java/org/SEP4_Data/Sep4DataApplication.java
@@ -0,0 +1,13 @@
+package org.SEP4_Data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep4DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep4DataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/SEP4_Data/Sep4DataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.SEP4_Data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep4DataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
